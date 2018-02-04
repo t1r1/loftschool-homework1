1b4b6097ff9038099b0855c34358b46a13a7ae5f
Maria Vorobyeva <tirimaria@gmail.com>
2018-02-04 20:47:27 +0300

    перенос файлов ДЗ5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import createStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
