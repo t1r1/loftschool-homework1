5103341bfb408c5d4923342266e56f8b50f7b6f7
Maria Vorobyeva <tirimaria@gmail.com>
2018-02-15 19:26:47 +0300

    скопированы файлы ДЗ6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AppRouter from './components/AppRouter';
+import getStore from './store';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = getStore();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
